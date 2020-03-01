962140493c5a6d4de6568883e8436f2eff79469b
Netmme <olivier.decasanove@net-c.fr>
2020-03-01 17:08:50 +0100

    First stone to a decent script.

---
+++ rename-mails/renameall.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+USER="$1"
+WORK_DIR="$PWD"
+DATA_DIR="$WORK_DIR/data"
+TMP_DIR="$WORK_DIR/tmp"
+
+echo "Starting renaming!"
+mkdir "${TMP_DIR}"
+
+while IFS="" read -r depot;
+do
+	echo "Cloning ${depot}..."
+	depot_dir="$TMP_DIR/$depot"
+	git clone --bare "https://github.com/${USER}/${depot}" "${depot_dir}"
+	cp ./rename-mails/gitscript.sh "${depot_dir}"
+	cd "${depot_dir}"
+
+	echo "Renaming ${depot}..."
+	./gitscript.sh
+	git push --force --tags origin 'refs/heads/*'
+	cd $WORK_DIR
+
+	echo "${depot} renamed!"
+done < $DATA_DIR/repositories.txt
+
+echo "Removing depositories..."
+rm -rf $TMP_DIR
+
+echo "...Ending renaming"
